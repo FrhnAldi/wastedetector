d3b8ca53d9fc7319c80284ab6dc36a5a4d018d60
FrhnAldi <farhanaldi03@gmail.com>
2026-05-18 22:50:35 +0700

    update env

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'WasteGuard — Deteksi Sampah B3')</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
