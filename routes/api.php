70643ee9514002d6bc929563bcdaa0e93189cf2f
FrhnAldi <farhanaldi03@gmail.com>
2026-05-19 00:30:16 +0700

    fix: restore routes/api.php content

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,7 @@
 <?php
 
-chdir(__DIR__ . '/../');
+use Illuminate\Support\Facades\Route;
 
-define('LARAVEL_START', microtime(true));
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+Route::get('/test', function () {
+    return response()->json(['status' => 'ok']);
+});
